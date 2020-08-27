7efbc42da5132f4f8d6f8a7dbf06ec5da86d0bf7
Emre Alper <emrealper@gmail.com>
2020-08-27 08:46:17 +0300

    Procurement Model is added

---
+++ SeparatedTextFileReader.Domain/Entities/Procurement.cs
@@ -0,0 +1,26 @@
+﻿using SeparatedTextFileReader.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeparatedTextFileReader.Domain.Entities
+{
+   public class Procurement :IEntity
+    {
+
+        public int Project { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public string Category { get; set; }
+
+        public string Responsible { get; set; }
+
+        public decimal? SavingsAmount { get; set; }
+
+        public string Currency { get; set; }
+
+        public string Complexity { get; set; }
+
+    }
+}
---
+++ SeparatedTextFileReader.Domain/Enums/ComplexityTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeparatedTextFileReader.Domain.Enums
+{
+    public enum ComplexityTypeEnum
+    {
+        Simple=1,
+        Moderate=2,
+        Hazardous=3
+
+    }
+}
